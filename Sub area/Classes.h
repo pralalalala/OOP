6409964e026130299d0c87ebd688bfaae5a2e078
pralalalala <68529744+pralalalala@users.noreply.github.com>
2023-10-10 11:00:43 +1000

    Update Classes.h

--- Sub area/Classes.h
+++ Sub area/Classes.h
@@ -21,4 +21,46 @@ public:
 	};
 
 
+	class Battle_Move_Assets
+	{
+	public:
+		virtual void Mag_attack() = 0;
+		virtual void Phis_attack() = 0;
+		virtual bool Block_Attack() = 0;
+		virtual bool Dodge_Attack() = 0;
+	};
+
+	class Move_Results : public Battle_Move_Assets
+	{
+		int HP, Stamina;
+	public:
+		/*virtual*/
+		bool Block_Attack()
+		{
+			if (Stamina > 10) return true;
+			else return false;
+		}
+
+		bool Dodge_Attack()
+		{
+			if (HP > 30) return true;
+			else return false;
+		}
+	};
+
+	class Decorator : public Battle_Move_Assets  
+	{
+		Battle_Move_Assets* BMA;
+	public:
+		Decorator(Battle_Move_Assets* w)
+		{
+			BMA = w;
+		}
+
+		/*virtual*/
+		bool Dodge_Attack()
+		{
+			BMA->Dodge_Attack(); // делегирование
+		}
+	};
 };
